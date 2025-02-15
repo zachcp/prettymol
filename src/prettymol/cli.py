9f612f63ccf562e854b5da6b4572b7beaadff0db
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-02-15 12:24:07 -0500

    update the cli

--- src/prettymol/cli.py
+++ src/prettymol/cli.py
@@ -11,11 +11,18 @@ from .repltools import Repltools
 @click.option('--output', required=True, help='Output file path for the rendered image')
 def cli(code, output):
     """Generate molecular structure image from code"""
+
+    # load structure
     struct = load_pdb(code)
 
-    rt = Repltools()  # does the basic setup
+    # setup the scene
+    rt = Repltools()
+    rt.set_view_axis(2)
+
+    # draw the molecule
     draw(struct, CartoonStyle(), Material())
 
+    # render the scene
     bpy.context.scene.render.filepath = output
     bpy.ops.render.render(use_viewport=True, write_still=True)
     return "Done"
