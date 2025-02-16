30e9ecb6569141c8ada2bc3c88976da48959f35c
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-02-16 15:38:36 -0500

    still not working

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,9 +6,6 @@ from click.testing import CliRunner
 
 def test_hello_command():
     runner = CliRunner()
-    result = runner.invoke(cli, ['1fap', 'test.png'])
-    assert os.path.exists('test.png')
-    try:
-        os.remove('test.png')
-    except:
-        pass
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ['1fap', 'test.png'])
+        assert os.path.exists('test.png')
