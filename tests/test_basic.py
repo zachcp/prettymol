574cb1e7b487340b9dd3c88136c7506abb724788
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-02-16 15:36:08 -0500

    fix imports

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,11 +3,12 @@ import pytest
 def test_imports():
     try:
         import prettymol
+        from prettymol import draw, load_pdb
         from prettymol import Repltools
         from prettymol import StructureSelector
-        from prettymol import draw, load_pdb
-        from prettymol import Material
-        from prettymol import CartoonStyle
+        from prettymol import MaterialCreator
+        from prettymol import StyleCreator
+        from prettymol import LightingCreator
         assert True
     except ImportError as e:
         pytest.fail(f"Import test failed: {e}")
