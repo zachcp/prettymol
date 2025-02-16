903c00abf4316f126d58f838a605378c264ba48b
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-02-16 15:41:04 -0500

    v0.2.0 and tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,9 @@
 import os
 from prettymol.cli import cli
-
 from click.testing import CliRunner
-
 
 def test_hello_command():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(cli, ['1fap', 'test.png'])
+        result = runner.invoke(cli, ['--code', '1fap', '--output', 'test.png'])
         assert os.path.exists('test.png')
