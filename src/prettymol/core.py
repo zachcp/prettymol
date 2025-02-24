c5821cac226b0bdb82c81b1dd68085031951a27a
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-02-24 13:00:01 -0500

    refactor the starting tree to not use the base colors. This will allow us to pass in the colors via FN before the object is created.

--- src/prettymol/core.py
+++ src/prettymol/core.py
@@ -1,4 +1,5 @@
 import bpy
+import databpy
 import numpy as np
 from typing import Union, Any
 from biotite.structure.io import pdbx
@@ -6,9 +7,50 @@ from biotite.structure import bonds
 from molecularnodes.entities.molecule.base import _create_object
 from molecularnodes.download import download
 from molecularnodes.blender import nodes as bl_nodes
+from molecularnodes.blender.nodes import add_custom, get_input, get_output,get_mod, new_tree, styles_mapping
 
 from .materials import Material, MaterialCreator
 from .styles import BallStickStyle, CartoonStyle, RibbonStyle, SpheresStyle, SticksStyle, SurfaceStyle
+
+
+
+# Modified form the original:
+# https://github.com/BradyAJohnston/MolecularNodes/blob/main/molecularnodes/blender/nodes.py
+# removes the color nodes to make a dead-simple node.
+def create_starting_node_tree_minimal(
+    object: bpy.types.Object,
+    coll_frames: bpy.types.Collection | None = None,
+    style: str = "spheres",
+    name: str | None = None,
+    color: str = "common",
+    material: str = "MN Default",
+    is_modifier: bool = True,
+) -> None:
+    mod = get_mod(object)
+    if not name:
+        name = f"MN_{object.name}"
+
+    try:
+        tree = bpy.data.node_groups[name]
+        mod.node_group = tree
+        return
+    except KeyError:
+        pass
+
+    tree = new_tree(name, input_name="Atoms")
+    tree.is_modifier = is_modifier
+    link = tree.links.new
+    mod.node_group = tree
+
+    # move the input and output nodes for the group
+    node_input = get_input(tree)
+    node_output = get_output(tree)
+    node_input.location = [0, 0]
+    node_output.location = [700, 0]
+    node_style = add_custom(tree, styles_mapping[style], [450, 0], material=material)
+    link(node_style.outputs[0], node_output.inputs[0])
+    link(node_input.outputs[0], node_style.inputs[0])
+    return None
 
 
 # Connect bonds and center the structure
@@ -23,19 +65,13 @@ def load_pdb(code):
 
 StyleType = Union[BallStickStyle, CartoonStyle, RibbonStyle, SpheresStyle, SticksStyle, SurfaceStyle]
 
-def draw(arr: Any, style: StyleType, material: Material) -> None:
+# ARR + Styles + Materials (+ Color Map....)
+def draw(arr: Any, style: StyleType, material: Material):
 
     # Create object and material
     molname = f"mol_{id(arr)}"
     matname = f"mol_{id(arr)}_mat"
     obj, _ = _create_object(arr, name=molname)
-    bl_nodes.create_starting_node_tree(obj, style=style.style)
-
-    # Setup node tree
-    modifier = next(mod for mod in obj.modifiers if mod.type == "NODES")
-    node_tree = modifier.node_group
-    nodes = node_tree.nodes
-    style_node = next((node for node in nodes if "Style" in node.name), None)
 
     # Create and setup material
     mat = bpy.data.materials.new(matname)
@@ -46,22 +82,19 @@ def draw(arr: Any, style: StyleType, material: Material) -> None:
             if value := material.get_by_key(input.name):
                         input.default_value = value
 
-    # Assign material to object
-    if obj.data.materials:
-        obj.data.materials[0] = mat
-    else:
-        obj.data.materials.append(mat)
+    # create the onject with the tyles and material
+    create_starting_node_tree_minimal(obj, style=style.style, material=mat)
 
-    # Apply style and materials
+    # Setup node tree
+    modifier = next(mod for mod in obj.modifiers if mod.type == "NODES")
+    node_tree = modifier.node_group
+    nodes = node_tree.nodes
+    style_node = next((node for node in nodes if "Style" in node.name), None)
+
+    # Apply style overrides
     if style_node:
         for input in style_node.inputs:
             if input.type != "GEOMETRY":
                 if value := style.get_by_key(input.name):
                     input.default_value = value
-
-        # Link material to style node
-        material_input = next((inp for inp in style_node.inputs if inp.name == "Material"), None)
-        if material_input:
-            material_input.default_value = mat
-
     return obj
